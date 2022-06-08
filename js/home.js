6588cce86a3ff795fa3384327c244f107f2ad7d7
Aman Rauniyar <amanrauniyar@outlook.com>
2022-06-08 10:25:08 -0400

    Removed the files no longer needed

--- js/home.js
+++ js/home.js
@@ -1,5 +1,13 @@
-let moon = document.getElementById('moon');
-    let moon_mountain = document.getElementById('moon_mountain');
-    let headline = document.getElementById('headline');
-    let btn = document.getElementById('btn');
-    let alienstarship = document.getElementById('alienstarship');
+// let moon = document.getElementById('moon');
+//     let moon_mountain = document.getElementById('moon_mountain');
+//     let headline = document.getElementById('headline');
+//     let btn = document.getElementById('btn');
+//     let alienstarship = document.getElementById('alienstarship');
+
+var h1 = ["Kids", " World"];
+var colors = ["orange", "green"];
+var text = "";
+    for (var i = 0; i < h1.length; i++) {
+    text += "<font color='" + colors[i] + "'>" + h1[i] + "</font>";
+    }
+document.getElementById("demo").innerHTML = text;
