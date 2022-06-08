bb2f514b662ffac8053088df16716093e61a9189
Aman Rauniyar <amanrauniyar@outlook.com>
2022-06-08 10:31:17 -0400

    Removed excess code

--- js/home.js
+++ js/home.js
@@ -1,9 +1,3 @@
-// let moon = document.getElementById('moon');
-//     let moon_mountain = document.getElementById('moon_mountain');
-//     let headline = document.getElementById('headline');
-//     let btn = document.getElementById('btn');
-//     let alienstarship = document.getElementById('alienstarship');
-
 var h1 = ["Kids", " World"];
 var colors = ["orange", "green"];
 var text = "";
